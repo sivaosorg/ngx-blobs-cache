9eab08226b8b393cbd0cd6e4a7a0ce94eb634e60
Nguyen Huu Phuoc <nguyenhuuphuocit97@gmail.com>
2022-02-12 11:05:18 +0700

    :ok_hand: chore: add redis styles config bean #6

--- src/main/java/com/phuocnguyen/app/ngxblobscache/config/ServicesCachesGlobalConfig.java
+++ src/main/java/com/phuocnguyen/app/ngxblobscache/config/ServicesCachesGlobalConfig.java
@@ -1,11 +1,15 @@
 package com.phuocnguyen.app.ngxblobscache.config;
 
 import com.phuocnguyen.app.ngxblobssrv.service.NgxRedisBaseService;
+import com.phuocnguyen.app.ngxblobssrv.service.NgxRedisStylesBaseService;
 import com.phuocnguyen.app.ngxblobssrv.service.serviceImpl.NgxRedisBaseServiceImpl;
+import com.phuocnguyen.app.ngxblobssrv.service.serviceImpl.NgxRedisStylesBaseServiceImpl;
 import com.sivaos.config.propertiesConfig.RedisPubSubProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
 
@@ -13,11 +17,21 @@ import javax.annotation.Resource;
 @Configuration
 public class ServicesCachesGlobalConfig {
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Bean
     @Primary
     @Resource(name = "ngxRedisBaseService")
     public NgxRedisBaseService ngxRedisBaseService() {
         return new NgxRedisBaseServiceImpl();
+    }
+
+    @Bean
+    @Primary
+    @Resource(name = "ngxRedisStylesBaseService")
+    public NgxRedisStylesBaseService ngxRedisStylesBaseService() {
+        return new NgxRedisStylesBaseServiceImpl(ngxRedisBaseService(), redisTemplate);
     }
 
     @Bean
